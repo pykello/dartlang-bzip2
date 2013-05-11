7d4d081520536519c924aeee824724a04b2b6582
Hadi Moshayedi <hadi@moshayedi.net>
2013-05-11 07:00:36 +0300

    huffmandecoder readability improved

--- lib/src/huffmandecoder.dart
+++ lib/src/huffmandecoder.dart
@@ -3,70 +3,70 @@ part of bzip2;
 const int _TABLE_BITS  = 9;
 
 class _HuffmanDecoder {
-  int _maxBitCount;
+  int _maxLength;
   int _symbolCount;
-  List<int> limits;
-  List<int> positions;
+  List<int> maxValueWithLength;
+  List<int> minPosWithLength;
   List<int> symbols;
   List<int> lengths;
   
-  _HuffmanDecoder(this._maxBitCount, this._symbolCount) {
-    limits = new List<int>(_maxBitCount + 1);
-    positions = new List<int>(_maxBitCount + 1);
+  _HuffmanDecoder(this._maxLength, this._symbolCount) {
+    maxValueWithLength = new List<int>(_maxLength + 1);
+    minPosWithLength = new List<int>(_maxLength + 1);
     symbols = new List<int>(_symbolCount);
     lengths = new List<int>(1 << _TABLE_BITS);
   }
   
   bool setCodeLengths(List<int> codeLengths) {
-    List<int> lenCounts = new List<int>.filled(_maxBitCount + 1, 0);
-    List<int> tmpPositions = new List<int>(_maxBitCount + 1);
-    for (int symbol = 0; symbol < _symbolCount; symbol++)
-    {
-      int len = codeLengths[symbol];
-      if (len > _maxBitCount)
-        return false;
-      lenCounts[len]++;
-      symbols[symbol] = 0xFFFFFFFF;
+    List<int> lengthCount = new List<int>.filled(_maxLength + 1, 0);
+    for (int symbol = 0; symbol < _symbolCount; symbol++) {
+      lengthCount[codeLengths[symbol]]++;    
     }
-    lenCounts[0] = 0;
-    positions[0] = limits[0] = 0;
-    int startPos = 0;
-    int index = 0;
-    int kMaxValue = (1 << _maxBitCount);
-    for (int i = 1; i <= _maxBitCount; i++)
-    {
-      startPos += lenCounts[i] << (_maxBitCount - i);
-      if (startPos > kMaxValue)
+    
+    minPosWithLength[0] = 0;
+    maxValueWithLength[0] = -1;
+    int startValue = 0;
+    int treeSize = (1 << _maxLength);
+    
+    for (int length = 1; length <= _maxLength; length++) {
+      int subtreeSize = 1 << (_maxLength - length);
+      int valueCount = lengthCount[length] * subtreeSize;
+      maxValueWithLength[length] = startValue + valueCount - 1;
+      minPosWithLength[length] = minPosWithLength[length - 1] + lengthCount[length - 1];
+      
+      startValue += valueCount;
+      if (startValue > treeSize)
         return false;
-      limits[i] = (i == _maxBitCount) ? kMaxValue : startPos;
-      positions[i] = positions[i - 1] + lenCounts[i - 1];
-      tmpPositions[i] = positions[i];
-      if(i <= _TABLE_BITS)
-      {
-        int limit = (limits[i] >> (_maxBitCount - _TABLE_BITS));
-        for (; index < limit; index++)
-          lengths[index] = i;
+    }
+    
+    int index = 0;
+    for (int i = 1; i <= _TABLE_BITS; i++) {
+      int limit = ((maxValueWithLength[i] + 1) >> (_maxLength - _TABLE_BITS));
+      for (; index < limit; index++)
+        lengths[index] = i;
+    }
+    
+    List<int> nextPosWithLength = new List<int>.from(minPosWithLength);
+    for (int symbol = 0; symbol < _symbolCount; symbol++) {
+      int length = codeLengths[symbol];
+      if (length != 0) {
+        symbols[nextPosWithLength[length]++] = symbol;
       }
     }
-    for (int symbol = 0; symbol < _symbolCount; symbol++)
-    {
-      int len = codeLengths[symbol];
-      if (len != 0)
-        symbols[tmpPositions[len]++] = symbol;
-    }
+    
     return true;
   }
   
   int decodeSymbol(BitBuffer buffer) {
-    int numBits;
-    int value = buffer.peekBits(_maxBitCount);
-    if (value < limits[_TABLE_BITS])
-      numBits = lengths[value >> (_maxBitCount - _TABLE_BITS)];
+    int length;
+    int value = buffer.peekBits(_maxLength);
+    if (value < (maxValueWithLength[_TABLE_BITS] + 1))
+      length = lengths[value >> (_maxLength - _TABLE_BITS)];
     else
-      for (numBits = _TABLE_BITS + 1; value >= limits[numBits]; numBits++);
-    buffer.move(numBits);
-    int index = positions[numBits] +
-      ((value - limits[numBits - 1]) >> (_maxBitCount - numBits));
+      for (length = _TABLE_BITS + 1; value > maxValueWithLength[length]; length++);
+    buffer.move(length);
+    int index = minPosWithLength[length] +
+      ((value - (maxValueWithLength[length - 1] + 1)) >> (_maxLength - length));
     if (index >= _symbolCount)
       return 0xFFFFFFFF;
     return symbols[index];
